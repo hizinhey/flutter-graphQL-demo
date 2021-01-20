70c53539078979b029c21e0b47faf7a916b335d6
Nghia Ngo <nghiangominh99@gmail.com>
2021-01-20 23:26:26 +0700

    Complete

--- project/lib/main.dart
+++ project/lib/main.dart
@@ -13,16 +13,17 @@ String query = """{
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  HttpLink httpLink = HttpLink(
-    uri: 'http://localhost:9000/graphql',
+  final HttpLink httpLink = HttpLink(
+    uri: "http://192.168.1.10:9000/graphql",
   );
 
-  AuthLink authLink = AuthLink(
+  final AuthLink authLink = AuthLink(
     getToken: () async => 'Bearer <YOUR_PERSONAL_ACCESS_TOKEN>',
+    // OR
+    // getToken: () => 'Bearer <YOUR_PERSONAL_ACCESS_TOKEN>',
   );
 
-  Link link = authLink.concat(httpLink);
-
+  final Link link = authLink.concat(httpLink);
   client = ValueNotifier(
     GraphQLClient(
       cache: InMemoryCache(),
@@ -33,6 +34,8 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  _handleAdd(String body) {}
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
@@ -53,17 +56,55 @@ class Home extends StatefulWidget {
   _HomeState createState() => _HomeState();
 }
 
-_buildItem(String id, String body, String created) {
+String delete = """
+          mutation deleteTodo(\$todoId: ID!){
+            deleteTodo(todoId: \$todoId) {
+                
+            }
+        }
+        """;
+
+String add = """
+          mutation createTodo(\$body: String!){
+            createTodo(body: \$body) {
+                body
+            }
+        }
+        """;
+
+_buildItem(String todoId, String body) {
   return Container(
-      child: Row(
-    children: [
-      IconButton(
-        icon: Icon(Icons.remove_circle_outline),
-        onPressed: () {},
-      ),
-      Text(body)
-    ],
-  ));
+    child: Row(
+      children: [
+        Mutation(
+          options: MutationOptions(
+            documentNode:
+                gql(delete), // this is the mutation string you just created
+            // you can update the cache based on results
+            update: (Cache cache, QueryResult result) {
+              return cache;
+            },
+            // or do something with the result.data on completion
+            onCompleted: (dynamic resultData) {
+              print(resultData);
+            },
+          ),
+          builder: (
+            RunMutation runMutation,
+            QueryResult result,
+          ) {
+            return FlatButton(
+              onPressed: () => runMutation({
+                'todoId': todoId,
+              }),
+              child: Icon(Icons.remove_circle),
+            );
+          },
+        ),
+        Text(body)
+      ],
+    ),
+  );
 }
 
 class _HomeState extends State<Home> {
@@ -80,14 +121,36 @@ class _HomeState extends State<Home> {
               });
             },
           ),
-          FlatButton(
-            child: Text("Add"),
-            onPressed: () {},
+          Mutation(
+            options: MutationOptions(
+              documentNode:
+                  gql(add), // this is the mutation string you just created
+              // you can update the cache based on results
+              update: (Cache cache, QueryResult result) {
+                return cache;
+              },
+              // or do something with the result.data on completion
+              onCompleted: (dynamic resultData) {
+                print(resultData);
+              },
+            ),
+            builder: (
+              RunMutation runMutation,
+              QueryResult result,
+            ) {
+              return FlatButton(
+                onPressed: () => runMutation({
+                  'body': textField,
+                }),
+                child: Text("ADD"),
+              );
+            },
           ),
           Query(
             options: QueryOptions(
               documentNode:
                   gql(query), // this is the query string you just created
+              variables: {},
               pollInterval: 10,
             ),
             // Just like in apollo refetch() could be used to manually trigger a refetch
@@ -95,7 +158,6 @@ class _HomeState extends State<Home> {
             builder: (QueryResult result,
                 {VoidCallback refetch, FetchMore fetchMore}) {
               if (result.hasException) {
-                print(result.exception.toString());
                 return Text(result.exception.toString());
               }
 
@@ -104,16 +166,16 @@ class _HomeState extends State<Home> {
               }
 
               // it can be either Map or List
+
               List repositories = result.data['getTodos'];
-
-              return ListView.builder(
-                  itemCount: repositories.length,
-                  itemBuilder: (context, index) {
-                    final repository = repositories[index];
-
-                    return _buildItem(repository['id'], repository["body"],
-                        repository["created"]);
-                  });
+              return Container(
+                  height: 300,
+                  child: ListView.builder(
+                      itemCount: repositories.length,
+                      itemBuilder: (context, index) {
+                        final repository = repositories[index];
+                        return _buildItem(repository['id'], repository['body']);
+                      }));
             },
           )
         ],
